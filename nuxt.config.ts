b44db0f6005cb88a9fe84e69eecc0203d56edc97
JJ Teoh <teohjjteoh@gmail.com>
2024-04-15 10:53:15 +0800

    migrate firebase config to env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,15 @@ export default defineNuxtConfig({
   css: ['primevue/resources/themes/aura-light-green/theme.css'],
   vuefire: {
     auth: { enabled: true },
+    // config: {
+    //   apiKey: process.env.FIREBASE_API_KEY,
+    //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    //   projectId: process.env.FIREBASE_PROJECT_ID,
+    //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    //   appId: process.env.FIREBASE_APP_ID,
+    //   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    // },
     config: {
       apiKey: 'AIzaSyAvTFnFWFKpXmF0SLIkMNbHHZtudxkz0Zg',
       authDomain: 'grocery-share-d71c3.firebaseapp.com',
